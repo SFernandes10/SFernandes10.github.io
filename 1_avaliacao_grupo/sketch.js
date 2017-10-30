12ad5bc40c8eb0a002ff5c623fbf9c34f8922694
SFernandes10 <futsal@live.com.pt>
2017-10-30 16:33:51 +0000

    up 1

--- 1_avaliacao_grupo/sketch.js
+++ 1_avaliacao_grupo/sketch.js
@@ -1,18 +1,60 @@
 
-
+var x, y, t, v, factorCrescimento, xAnterior, yAnterior, pix;
+var img;
+function preload() {
+  img = loadImage("imagem/EVA.png");
+}
 function setup() 
 {
-  createCanvas (windowWidth, windowHeight);
+  createCanvas (596, 842, SVG);
+  
+  image(img, 0, 0);
+  x = width/2;
+  y = height/2;
+  xAnterior = x;
+  yAnterior = y;
+
+  v = 30;
+  t = v;
+  factorCrescimento = 2;
+ 
 }
 
 
 function draw() 
 {
-  background( random(0, 255), random(0, 255), random(0, 255) );
+  if (frameCount%50 === 0) background (255,10);
+  x += random(-v, v);
+  y += random(-v, v);
+
+  if (x<=0) x +=t;
+  if (x>=width) x-=t;
+  if (y<=0) y+=t;
+  if (y>=height) y-=t;
+
+  if (t>30 ||t<0) factorCrescimento*=-1;
+ 
+
+  t +=factorCrescimento;
+ 
+  var pix = img.get(x,y);
+  noFill();
+  stroke (pix, 128);
+  line(xAnterior, yAnterior, x, y);
+
+  xAnterior = x;
+  yAnterior = y;
 }
 
 
-function windowResized() 
+function keyPressed()
 {
-  resizeCanvas (windowWidth, windowHeight);
+  if (key === "1")
+  {
+    noLoop();
+    save("meuProjecto.svg");
+
+  }
+
+
 }
